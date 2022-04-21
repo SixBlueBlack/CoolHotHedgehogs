e77315f870a4418d41e8603c3902234901947137
Mark <sophirenm@gmail.com>
2022-04-21 18:33:21 +0300

    Room generation with rimm's spanning minimal random tree

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -7,168 +7,6 @@ namespace Completed
 {
     public class BoardManager : MonoBehaviour
     {
-        public class OuterWall
-        {
-            public enum Orientations
-            {
-                Horizontal,
-                Vertical
-            }
-            public int Length { get; }
-            public Orientations Orientation { get; }
-            public GameObject[] Tiles { get; }
-            public bool HasPath { get; }
-
-            public OuterWall(int length, Orientations orientation, bool hasPath, GameObject[] tiles)
-            {
-                Length = length;
-                HasPath = hasPath;
-                Orientation = orientation;
-                Tiles = tiles;
-            }
-
-            public void Generate(Vector3 offset)
-            {
-                for (var i = 0; i < Length; i++)
-                    if (!HasPath || (i != 4 && i != 5))
-                        if (Orientation == Orientations.Horizontal)
-                            Instantiate(Tiles[Random.Range(0, Tiles.Length)],
-                                new Vector3(i, 0, 0f) + offset, Quaternion.identity);
-                        else
-                            Instantiate(Tiles[Random.Range(0, Tiles.Length)],
-                                new Vector3(0, i, 0f) + offset, Quaternion.identity);
-            }
-        }
-
-        public class Board
-        {
-            public Room[,] Field { get; }
-            public GameObject[] FloorTiles { get; }
-            public GameObject[] OuterWallTiles { get; }
-            public GameObject[] WallTiles { get; }
-            public int Rows { get; }
-            public int Columns { get; }
-            public int RoomRows { get; }
-            public int RoomColumns { get; }
-
-            public Board(int rows, int columns, int roomRows, int roomColumns,
-                GameObject[] floorTiles, GameObject[] outerWallTiles, GameObject[] wallTiles)
-            {
-                Rows = rows;
-                Columns = columns;
-                RoomRows = roomRows;
-                RoomColumns = roomColumns;
-                FloorTiles = floorTiles;
-                OuterWallTiles = outerWallTiles;
-                WallTiles = wallTiles;
-
-                Field = new Room[rows, columns];
-                var possiblePathsNumber = new int[rows, columns];
-
-                for (var i = 0; i < rows; i++)
-                    for (var j = 0; j < columns; j++)
-                    {
-                        var paths = 4;
-                        if (j == 0 || j == rows - 1)
-                            paths--;
-                        if (i == 0 || i == columns - 1)
-                            paths--;
-                        possiblePathsNumber[i, j] = paths;
-                    }
-
-                for (var i = 0; i < rows; i++)
-                {
-                    for (var j = 0; j < columns; j++)
-                    {
-                        var upperWall = new OuterWall(roomColumns, OuterWall.Orientations.Horizontal,
-                            RandomizePath(i, j, i + 1, j, possiblePathsNumber), OuterWallTiles);
-                        var rightWall = new OuterWall(roomColumns, OuterWall.Orientations.Vertical,
-                            RandomizePath(i, j, i, j + 1, possiblePathsNumber), OuterWallTiles);
-                        OuterWall bottomWall;
-                        if (i != 0)
-                            bottomWall = Field[i - 1, j].UpperWall;
-                        else
-                            bottomWall = new OuterWall(roomColumns, OuterWall.Orientations.Horizontal,
-                                RandomizePath(i, j, i - 1, j, possiblePathsNumber), OuterWallTiles);
-                        OuterWall leftWall;
-                        if (j != 0)
-                            leftWall = Field[i, j - 1].RightWall;
-                        else
-                            leftWall = new OuterWall(roomColumns, OuterWall.Orientations.Vertical,
-                            RandomizePath(i, j, i, j - 1, possiblePathsNumber), OuterWallTiles);
-
-                        Field[i, j] = new Room(roomRows, roomColumns, floorTiles, outerWallTiles,
-                            bottomWall, upperWall, rightWall, leftWall);
-                    }
-                }
-            }
-
-            private bool RandomizePath(int iFrom, int jFrom, int iTo, int jTo, int[,] possiblePathsNumber)
-            {
-                if (iTo >= Rows || jTo >= Columns || iTo < 0 || jTo < 0)
-                    return false;
-
-                var randomCoef = 1.0 / Math.Min(possiblePathsNumber[iTo, jTo], possiblePathsNumber[iFrom, jFrom]);
-                if (Math.Abs(1 - randomCoef) > 1e-5)
-                    randomCoef *= 0.5;
-                var hasPath = Random.value <= randomCoef;
-
-                possiblePathsNumber[iTo, jTo]--;
-                possiblePathsNumber[iFrom, jFrom]--;
-
-                return hasPath;
-            }
-
-            public void Generate()
-            {
-                for (var i = 0; i < Rows; i++)
-                    for (var j = 0; j < Columns; j++)
-                        Field[i, j].Generate(new Vector3(j * RoomColumns, i * RoomRows, 0f));
-            }
-        }
-
-        public class Room
-        {
-            public OuterWall BottomWall { get; }
-            public OuterWall UpperWall { get; }
-            public OuterWall RightWall { get; }
-            public OuterWall LeftWall { get; }
-
-            public int Rows { get; }
-            public int Columns { get; }
-            GameObject[] FloorTiles { get; }
-            GameObject[] WallTiles { get; }
-
-            public Room(int rows, int columns, GameObject[] floorTiles, GameObject[] wallTiles,
-                OuterWall bottomWall, OuterWall upperWall, OuterWall rightWall, OuterWall leftWall)
-            {
-                FloorTiles = floorTiles;
-                WallTiles = wallTiles;
-
-                BottomWall = bottomWall;
-                UpperWall = upperWall;
-                RightWall = rightWall;
-                LeftWall = leftWall;
-
-                Rows = rows;
-                Columns = columns;
-            }
-
-            public void Generate(Vector3 offset)
-            {
-                BottomWall.Generate(offset);
-                LeftWall.Generate(offset + new Vector3(0, 1, 0));
-                RightWall.Generate(offset + new Vector3(Columns, 0, 0));
-                UpperWall.Generate(offset + new Vector3(1, Rows, 0));
-
-                for (var x = 0; x < Columns; x++)
-                    for (var y = 0; y < Rows; y++)
-                        Instantiate(FloorTiles[Random.Range(0, FloorTiles.Length)],
-                            new Vector3(x, y, 0f) + offset, Quaternion.identity);
-            }
-        }
-
-        // Using Serializable allows us to embed a class with sub properties in the inspector.
         [Serializable]
         public class Count
         {
@@ -192,36 +30,66 @@ namespace Completed
         public GameObject[] outerWallTiles; //Array of outer tile prefabs.
 
         //A list of possible locations to place tiles.
-        private List<Vector3> gridPositions = new List<Vector3>();
+        //private List<Vector3> gridPositions = new List<Vector3>();
 
 
-        Vector3 RandomPosition()
+        //Vector3 RandomPosition()
+        //{
+        //    int randomIndex = Random.Range(0, gridPositions.Count);
+        //    var randomPosition = gridPositions[randomIndex];
+        //    gridPositions.RemoveAt(randomIndex);
+
+        //    return randomPosition;
+        //}
+
+
+        //void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+        //{
+        //    int objectCount = Random.Range(minimum, maximum + 1);
+
+        //    for (int i = 0; i < objectCount; i++)
+        //    {
+        //        var randomPosition = RandomPosition();
+        //        var tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+        //        Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        //    }
+        //}
+
+        private void GenerateOuterWall(OuterWall wall, Vector3 offset)
         {
-            int randomIndex = Random.Range(0, gridPositions.Count);
-            var randomPosition = gridPositions[randomIndex];
-            gridPositions.RemoveAt(randomIndex);
-
-            return randomPosition;
+            for (var i = 0; i < wall.Length; i++)
+                if (!wall.HasPath || (i != 3 && i != 4))
+                    if (wall.Orientation == OuterWall.Orientations.Horizontal)
+                        Instantiate(outerWallTiles[Random.Range(0, outerWallTiles.Length)],
+                            new Vector3(i, 0, 0f) + offset, Quaternion.identity);
+                    else
+                        Instantiate(outerWallTiles[Random.Range(0, outerWallTiles.Length)],
+                            new Vector3(0, i, 0f) + offset, Quaternion.identity);
         }
 
-
-        void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+        private void GenerateRoom(Room room, Vector3 offset)
         {
-            int objectCount = Random.Range(minimum, maximum + 1);
+            GenerateOuterWall(room.BottomWall, offset);
+            GenerateOuterWall(room.LeftWall, offset + new Vector3(0, 1, 0));
+            GenerateOuterWall(room.RightWall, offset + new Vector3(room.Columns, 0, 0));
+            GenerateOuterWall(room.UpperWall, offset + new Vector3(1, room.Rows, 0));
 
-            for (int i = 0; i < objectCount; i++)
-            {
-                var randomPosition = RandomPosition();
-                var tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-                Instantiate(tileChoice, randomPosition, Quaternion.identity);
-            }
+            for (var x = 0; x < room.Columns; x++)
+                for (var y = 0; y < room.Rows; y++)
+                    Instantiate(floorTiles[Random.Range(0, floorTiles.Length)],
+                        new Vector3(x, y, 0f) + offset, Quaternion.identity);
         }
 
+        private void GenerateBoard(Board board)
+        {
+            for (var i = 0; i < board.Rows; i++)
+                for (var j = 0; j < board.Columns; j++)
+                    GenerateRoom(board.Field[i, j], new Vector3(j * board.RoomColumns, i * board.RoomRows, 0f));
+        }
 
         public void SetupScene()
         {
-            var board = new Board(rows, columns, 8, 8, floorTiles, outerWallTiles, wallTiles);
-            board.Generate();
+            GenerateBoard(new Board(rows, columns, 8, 8));
             // LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
         }
     }
---
+++ Assets/Scripts/OuterWall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Completed
+{
+    public class OuterWall
+    {
+        public enum Orientations
+        {
+            Horizontal,
+            Vertical
+        }
+        public int Length { get; }
+        public Orientations Orientation { get; }
+        public bool HasPath { get; }
+
+        public OuterWall(int length, Orientations orientation, bool hasPath)
+        {
+            Length = length;
+            HasPath = hasPath;
+            Orientation = orientation;
+        }
+    }
+}
