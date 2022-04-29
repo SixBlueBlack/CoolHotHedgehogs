9728c59c4c210810f8cb8fc967617082b206a28b
SixBlueBlack <me.mashkin7@gmail.com>
2022-04-29 09:18:21 +0500

    Add weapon panel without animation

---
+++ Assets/Scripts/WeaponPanelScript.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponPanelScript : MonoBehaviour
+{
+    public Image IconImage;
+
+    public int LastWeaponIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LastWeaponIndex = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(KeyCode.Alpha2.ToString());
+        if (LastWeaponIndex != PlayerWeaponScript.CurrentWeaponIndex)
+            IconImage.sprite = PlayerWeaponScript.Weapons[PlayerWeaponScript.CurrentWeaponIndex].Sprite;
+        LastWeaponIndex = PlayerWeaponScript.CurrentWeaponIndex;
+    }
+}
